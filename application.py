b2a8640a6f4c2f98bcb2ead5d78daaacb2405a33
Rishabh <84309625+Rishabh4-2-2002@users.noreply.github.com>
2023-12-18 22:04:09 +0530

    Update application.py

--- application.py
+++ application.py
@@ -17,18 +17,18 @@ application = Flask(__name__)
 
 
 
-def text_to_speech(text, language='en', filename='output.mp3'):
-    tts = gTTS(text=text, lang=language, slow=False)
-    tts.save(filename)
-    pygame.mixer.init()
-    pygame.mixer.music.load(filename,"mp3")
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
+# def text_to_speech(text, language='en', filename='output.mp3'):
+#     tts = gTTS(text=text, lang=language, slow=False)
+#     tts.save(filename)
+#     pygame.mixer.init()
+#     pygame.mixer.music.load(filename,"mp3")
+#     pygame.mixer.music.play()
+#     while pygame.mixer.music.get_busy():
+#         pygame.time.Clock().tick(10)
     
-    pygame.quit()
+#     pygame.quit()
     
-    os.remove('output.mp3')
+#     os.remove('output.mp3')
 ##########################################################################################################
 #                                       Main Code
 ##########################################################################################################
@@ -43,124 +43,125 @@ fontScale = 0.5
 color = (0,0,255)  #(B, G, R)
 thickness = 1
 
-def allCodeHere():
-    cap = cv2.VideoCapture(0)
-    detector = HandDetector(detectionCon=0.8, maxHands=2)
-    while True:
-        success, img = cap.read()
-        hands, img = detector.findHands(img)
-        cv2.putText(img, "a) Face Detection", (50, 50),  font, 1,  color,  2,  cv2.LINE_4)
-        cv2.putText(img, "b) OCR", (50, 100),  font, 1,  color,  2,  cv2.LINE_4)
-        cv2.putText(img, "c) Object Detection", (50, 150),  font, 1,  color,  2,  cv2.LINE_4)
-        cv2.putText(img, "d) Face Registration", (50, 200),  font, 1,  color,  2,  cv2.LINE_4)
-        cv2.putText(img, "e) Exit", (50, 250),  font, 1,  color,  2,  cv2.LINE_4)
+# def allCodeHere():
+#     cap = cv2.VideoCapture(0)
+#     detector = HandDetector(detectionCon=0.8, maxHands=2)
+#     while True:
+#         success, img = cap.read()
+#         hands, img = detector.findHands(img)
+#         cv2.putText(img, "a) Face Detection", (50, 50),  font, 1,  color,  2,  cv2.LINE_4)
+#         cv2.putText(img, "b) OCR", (50, 100),  font, 1,  color,  2,  cv2.LINE_4)
+#         cv2.putText(img, "c) Object Detection", (50, 150),  font, 1,  color,  2,  cv2.LINE_4)
+#         cv2.putText(img, "d) Face Registration", (50, 200),  font, 1,  color,  2,  cv2.LINE_4)
+#         cv2.putText(img, "e) Exit", (50, 250),  font, 1,  color,  2,  cv2.LINE_4)
 
-        if hands:
-            # get Hand1
-            hand1 = hands[0]
-            lmList1 = hand1["lmList"]  # List of 21 Landmarks points
-            bbox1 = hand1["bbox"]  # Bounding Box info x,y,w,h
-            centerPoint1 = hand1["center"]  # center of the hand cx,cy
-            handType1 = hand1["type"]  # hand type Left or Right
+#         if hands:
+#             # get Hand1
+#             hand1 = hands[0]
+#             lmList1 = hand1["lmList"]  # List of 21 Landmarks points
+#             bbox1 = hand1["bbox"]  # Bounding Box info x,y,w,h
+#             centerPoint1 = hand1["center"]  # center of the hand cx,cy
+#             handType1 = hand1["type"]  # hand type Left or Right
 
-            fingers1 = detector.fingersUp(hand1)
-            cnt = fingers1[0]+fingers1[1]+fingers1[2]+fingers1[3]+fingers1[4]
-            # orders
-            if cnt==0:
-                print("STOP!")
-            else:
-                if handType1=='Right':
-                    if fingers1[1]==1 and fingers1[2]==0 and fingers1[3]==0 and fingers1[0]==0 and fingers1[4]==0:
-                        text = "Initializing Face Recognition model"
-                        text_to_speech(text, language='en')
-                        # if mark_your_attendance():
-                        #     print("No Closed")
-                        #     mark_your_attendance()
-                        # else:
-                        #     print("Yes Closed")
-                        #     allCodeHere()
-                    elif fingers1[1]==1 and fingers1[2]==1 and fingers1[3]==0 and fingers1[4]==0 and fingers1[0]==0:
-                        text = "Initializing OCR model"
-                        text_to_speech(text, language='en')
-                        # if Detect_OCR():
-                        #     print("No Closed")
-                        #     Detect_OCR()
-                        # else:
-                        #     print("Yes Closed")
-                        #     allCodeHere()
-                    elif fingers1[1]==1 and fingers1[2]==1 and fingers1[3]==1 and fingers1[4]==0 and fingers1[0]==0:
-                        text = "Initializing Object Detection"
-                        text_to_speech(text, language='en')    
-                        # if detect_objects():
-                        #     print("No Closed")
-                        #     detect_objects()
-                        # else:
-                        #     print("Yes Closed")
-                        #     allCodeHere()
-                elif handType1=='Left':
-                    if fingers1[0]==1 and fingers1[1]==1 and fingers1[2]==0 and fingers1[3]==0 and fingers1[4]==0:
-                        # cap.release()
-                        # cv2.destroyWindow("Hands") 
-                        text = "See you soon"
-                        text_to_speech(text, language='en')
-                        break
+#             fingers1 = detector.fingersUp(hand1)
+#             cnt = fingers1[0]+fingers1[1]+fingers1[2]+fingers1[3]+fingers1[4]
+#             # orders
+#             if cnt==0:
+#                 print("STOP!")
+#             else:
+#                 if handType1=='Right':
+#                     if fingers1[1]==1 and fingers1[2]==0 and fingers1[3]==0 and fingers1[0]==0 and fingers1[4]==0:
+#                         text = "Initializing Face Recognition model"
+#                         text_to_speech(text, language='en')
+#                         # if mark_your_attendance():
+#                         #     print("No Closed")
+#                         #     mark_your_attendance()
+#                         # else:
+#                         #     print("Yes Closed")
+#                         #     allCodeHere()
+#                     elif fingers1[1]==1 and fingers1[2]==1 and fingers1[3]==0 and fingers1[4]==0 and fingers1[0]==0:
+#                         text = "Initializing OCR model"
+#                         text_to_speech(text, language='en')
+#                         # if Detect_OCR():
+#                         #     print("No Closed")
+#                         #     Detect_OCR()
+#                         # else:
+#                         #     print("Yes Closed")
+#                         #     allCodeHere()
+#                     elif fingers1[1]==1 and fingers1[2]==1 and fingers1[3]==1 and fingers1[4]==0 and fingers1[0]==0:
+#                         text = "Initializing Object Detection"
+#                         text_to_speech(text, language='en')    
+#                         # if detect_objects():
+#                         #     print("No Closed")
+#                         #     detect_objects()
+#                         # else:
+#                         #     print("Yes Closed")
+#                         #     allCodeHere()
+#                 elif handType1=='Left':
+#                     if fingers1[0]==1 and fingers1[1]==1 and fingers1[2]==0 and fingers1[3]==0 and fingers1[4]==0:
+#                         # cap.release()
+#                         # cv2.destroyWindow("Hands") 
+#                         text = "See you soon"
+#                         text_to_speech(text, language='en')
+#                         break
 
 
-        cv2.imshow("Hands", img)
-        # Wait for Esc key to stop
-        k = cv2.waitKey(1) & 0xff
-        if k == 27:
-            break
+#         cv2.imshow("Hands", img)
+#         # Wait for Esc key to stop
+#         k = cv2.waitKey(1) & 0xff
+#         if k == 27:
+#             break
 
-    cap.release()
-    cv2.destroyAllWindows() 
+#     cap.release()
+#     cv2.destroyAllWindows() 
 
-@application.route("/recording", methods = ['POST', 'GET'])
-def recording():
-    if request.method == 'POST':
-        print("POST")
-        f2 = request.form.get('abc')
-        print("audio file")
-        print(f2)
+# @application.route("/recording", methods = ['POST', 'GET'])
+# def recording():
+#     if request.method == 'POST':
+#         print("POST")
+#         f2 = request.form.get('abc')
+#         print("audio file")
+#         print(f2)
 
-        allCodeHere()
+#         allCodeHere()
         
-        return render_template('services.html')
-    return render_template('services.html')
+#         return render_template('services.html')
+#     return render_template('services.html')
 
 
-@application.route("/faceRegister", methods = ['POST', 'GET'])
-def faceRegister():
-    if request.method == 'POST':
-        print("POST")
-        user_name = request.form.get('uName')
-        print("user_name: ",user_name)
+# @application.route("/faceRegister", methods = ['POST', 'GET'])
+# def faceRegister():
+#     if request.method == 'POST':
+#         print("POST")
+#         user_name = request.form.get('uName')
+#         print("user_name: ",user_name)
 
-        # register_yourself(user_name)
+#         # register_yourself(user_name)
 
         
-        return render_template('services.html')
-    return render_template('services.html')
-##########################################################################################################
-#                                               about
-##########################################################################################################
-@application.route("/about", methods = ['POST', 'GET'])
-def about():
-    username=session.get('uname')
-    return render_template('about.html')
-##########################################################################################################
-#                                               contact
-##########################################################################################################
-@application.route("/contact", methods = ['POST', 'GET'])
-def contact():
-    username=session.get('uname')
-    return render_template('contact.html',firstName=username)
+#         return render_template('services.html')
+#     return render_template('services.html')
+# ##########################################################################################################
+# #                                               about
+# ##########################################################################################################
+# @application.route("/about", methods = ['POST', 'GET'])
+# def about():
+#     username=session.get('uname')
+#     return render_template('about.html')
+# ##########################################################################################################
+# #                                               contact
+# ##########################################################################################################
+# @application.route("/contact", methods = ['POST', 'GET'])
+# def contact():
+#     username=session.get('uname')
+#     return render_template('contact.html',firstName=username)
 #########################################################################################################
 #                                       Home page
 #########################################################################################################
 @application.route("/")
 def root():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return "Hello World!"
 
 
 
